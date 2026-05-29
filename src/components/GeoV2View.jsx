da3be618bbdb0adbff358ac66a8156e36242e846
edutec <edutec.general@gmail.com>
2026-05-29 13:32:05 -0600

    fix(geo-v2): usar subdominio OSM para cumplir CSP

--- src/components/GeoV2View.jsx
+++ src/components/GeoV2View.jsx
@@ -1210,7 +1210,7 @@ export default function GeoV2View({ userRole }) {
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           <ZoomControl position="bottomright" />
           <MapMoveTracker onMove={onMapMove} />
